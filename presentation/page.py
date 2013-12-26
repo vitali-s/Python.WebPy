fa442e4bb21a5a5ab6a63b67e4c6afc0e6627bc8
vitali shcharbin <vitali.shcharbin@gmail.com>
2013-12-26 23:31:33 +0300

    Add webpy

--- presentation/page.py
+++ presentation/page.py
@@ -1,1 +1,16 @@
-__author__ = 'vitali_shcharbin'
+import web
+ 
+urls = (
+    '/', 'index'
+)
+ 
+class index:
+    def GET(self):
+        return "Hello, world!"
+
+def wsgiHandler():
+    return web.application(urls, globals(), autoreload=False).wsgifunc()
+ 
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
